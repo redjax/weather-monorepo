6b86263181d6374f31a3c39fa21eca7bb73d7c92
redjax <none@none.com>
2024-10-08 23:41:40 -0400

    Basic CLI app working.
    Verify app can connect to existing database from weathersched.
    
    Verify celery tasks run

--- apps/cli-app/src/cli_app/modules/demo/_cli.py
+++ apps/cli-app/src/cli_app/modules/demo/_cli.py
@@ -3,13 +3,13 @@ from cyclopts import App
 demo_app = App(name="demo", help="Demo functionality. Remove this eventually!")
 
 
-@demo_app.command
+@demo_app.command(group="demo")
 def bar(n: int):
     """Print the number, prepended with 'BAR: '."""
     print(f"BAR: {n}")
 
 
-@demo_app.command
+@demo_app.command(group="demo")
 def baz(n: int):
     """Print the number, prepended with 'BAZ: '."""
     print(f"BAZ: {n}")
